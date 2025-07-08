f47d73be8936e63dd5f4f533659ab40c652b562e
Faiz1615 <muhammadfaiz.satrio82@gmail.com>
2025-07-08 23:31:13 +0700

    -m 'Update Final Project'

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,22 +1,29 @@
 const homePage = require('../support/pageObjects/homePage/homePage');
 const signupPage = require('../support/pageObjects/signupPage/signupPage'); 
-const userData = require('../support/data/userData')
+const userData = require('../support/data/userData');
+const { beforeEach } = require('mocha');
 
 describe('Sign Up Feature', () => {
+  beforeEach(() =>{
+    homePage.goToHomePage();
+  })
+
+  afterEach(() =>{
+    signupPage.clickSignUpButton();
+  })
   it('User should be able to sign up with valid data', () => {
-   homePage.goToHomePage();
    homePage.clickSignUpMenu();
    signupPage.verifiySignUpPageAppears();
    signupPage.inputUsername(userData.valid_username);
    signupPage.inputPassword(userData.valid_password);
-   signupPage.clickSignUpButton();
+   
   })
+  
   it('User should be able to sign up with registered user', () => {
-   homePage.goToHomePage();
    homePage.clickSignUpMenu();
    signupPage.verifiySignUpPageAppears();
    signupPage.inputUsername(userData.invalid_username);
    signupPage.inputPassword(userData.invalid_password);
-   signupPage.clickSignUpButton();
+   
   })
 })
